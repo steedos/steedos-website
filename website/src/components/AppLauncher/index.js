de79f3894313ca792ebd31f2a892c1e56e02ecae
hotlong <zhuangjianguo@hotoa.com>
2020-03-14 12:46:10 +0800

    完善主页

--- website/src/components/AppLauncher/index.js
+++ website/src/components/AppLauncher/index.js
@@ -11,7 +11,7 @@ class SteedosAppLauncher extends React.Component {
 
 		return (
 						<div className="slds-app-launcher-container">
-							<AppLauncherExpandableSection title="应用程序" nonCollapsible={true}>
+							<AppLauncherExpandableSection title="华炎云平台" nonCollapsible={true}>
 								{/* <AppLauncherTile
 									description="SAAS 云端企业管理软件"
 									title="华炎云"
@@ -34,6 +34,16 @@ class SteedosAppLauncher extends React.Component {
 									/>}
 								/>
 								<AppLauncherTile
+									description="PAAS 企业应用开发平台"
+									title="开发平台"
+									href="/docs/platform/home"
+									iconNode={<Icon
+										category="standard"
+										name="system_and_global_variable"
+										size="large"
+									/>}
+								/>
+								<AppLauncherTile
 									description="合同审批、合同台账、统计分析"
 									title="合同管理"
 									href="/docs/solutions/contract"
@@ -44,22 +54,39 @@ class SteedosAppLauncher extends React.Component {
 									/>}
 								/>
 								<AppLauncherTile
+									description="通过更好的销售体验促进销量增长。"
+									title="销售管理"
+									iconNode={<Icon
+										category="standard"
+										name="opportunity"
+										size="large"
+									/>}
+								/>
+								<AppLauncherTile
+									description="管理供应商信息、生命周期、绩效和风险。"
+									title="供应商管理"
+									iconNode={<Icon
+										category="standard"
+										name="account"
+										size="large"
+									/>}
+								/>
+								<AppLauncherTile
+									description="全面了解并主动管理员工支出。"
+									title="差旅和费用"
+									iconNode={<Icon
+										category="standard"
+										name="client"
+										size="large"
+									/>}
+								/>
+								<AppLauncherTile
 									description="私有云即时通讯解决方案"
 									title="即时通讯"
 									href="/docs/products/messenger"
 									iconNode={<Icon
 										category="standard"
 										name="messaging_conversation"
-										size="large"
-									/>}
-								/>
-								<AppLauncherTile
-									description="低代码，极速构建企业应用"
-									title="开发平台"
-									href="/docs/platform/home"
-									iconNode={<Icon
-										category="standard"
-										name="system_and_global_variable"
 										size="large"
 									/>}
 								/>
