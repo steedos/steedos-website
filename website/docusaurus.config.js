fec25dfcb3a507e736147754685e850739a37da7
hotlong <zhuangjianguo@hotoa.com>
2020-03-26 20:00:28 +0800

    Update docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -112,7 +112,7 @@ module.exports = {
           items: [
             // {to: 'solutions/sales/home', label: '销售管理', position: 'left', activeBasePath: 'sales'},
             {to: 'platform/', label: '华炎开发平台', position: 'left', activeBasePath: 'platform/'},
-            {to: 'solutions/oa/', label: '华炎办公', position: 'left', activeBasePath: 'solutions/oa/'},
+            {to: 'solutions/workflow/', label: '审批王', position: 'left', activeBasePath: 'solutions/workflow/'},
           ]
         },
         {to: '', label: '解决方案', position: 'left',
@@ -121,8 +121,8 @@ module.exports = {
             {to: 'solutions/group/', label:'集团管控',position: 'left', activeBasePath: 'solutions/group/'},
             {to: 'solutions/cost/', label: '费控管理', position: 'left', activeBasePath: 'solutions/cost/'},
             {to: 'solutions/contract/', label: '合同管理', position: 'left', activeBasePath: 'solutions/contract/'},
-            {to: 'solutions/workflow/', label: '流程审批', position: 'left', activeBasePath: 'solutions/workflow/'},
             {to: 'solutions/knowledge/', label: '知识管理', position: 'left', activeBasePath: 'solutions/knowledge/'},
+            {to: 'solutions/oa/', label: '办公自动化', position: 'left', activeBasePath: 'solutions/oa/'},
           ]
         },
         {to: 'developer/', label: '开发文档', position: 'left', activeBasePath: 'developer/'},
